0cac7dd965168829ab802f3676bef6c06b9e4f46
ToastinYou <toastinyou@gmx.com>
2020-03-05 16:24:35 -0500

    Add air compressor subsystem & start.

---
+++ src/main/java/frc/robot/subsystems/AirSubsystem.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AirSubsystem extends SubsystemBase {
+  public static Compressor air;
+
+  public AirSubsystem() {
+    air = new Compressor(0);
+    air.setClosedLoopControl(false); // is this needed?
+    air.start();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
