7b44e7aa9d66a558371fc07e0dd017f5f9f8e0cd
ToastinYou <toastinyou@gmx.com>
2020-03-10 08:09:14 -0400

    Update color spinner to activate on DPAD Left.

--- src/main/java/frc/robot/commands/ColorCommand.java
+++ src/main/java/frc/robot/commands/ColorCommand.java
@@ -26,7 +26,7 @@ public class ColorCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (OI.getDpadRight()) {
+    if (OI.getDpadLeft()) {
       ColorSubsystem.ColorMotorController.set(Constants.kSpeedColor);
     }
     else {
